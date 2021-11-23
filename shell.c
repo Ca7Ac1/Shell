a735bb45ebdae738a5ab1a36cd8201fb2ce8fa2a
Wilde Chu <wildechu23@gmail.com>
2021-11-23 11:02:16 -0500

    added shell.c stuff

--- shell.c
+++ shell.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "parser.h"
 #include "shell.h"
@@ -8,24 +9,43 @@ void shell() {
 	while (true) {
 		char **tokens = parse();
 
-        for (int i = 0; i < struct.size; i++)
-        {
-        switch tokens[i]:
-		case "cd":
-			break;
-		case "exit:
-			return;
-		default:
-			int c = fork();
-			if(!c) execvp(tokens[0], tokens);
-			break;
-	    }
-    }
+	        for (int i = 0; i < struct.size; i++)
+	       	{
+			//create separate function
+			//int start = 0
+			for(int j = 0; j < struct[i].size; j++) {
+				//can u have multiple redirections
+				if(strcmp(struct[i][j], ">") == 0) {
+					rrun(tokens, j);	
+				}
+			}
+
+
+		       	if (strcmp(tokens[i], "cd") == 0) {
+	
+			} else if (strcmp(tokens[i], "exit") == 0) {
+				return;
+			} else {
+				run(tokens);
+			}
+    		}
+	}
 }
 
-void run()
+void run(token tokens)
 {
+	int c = fork();
+	if(!c) {
+		execvp(tokens[0], tokens);
+	}
+}
 
+void rrun(token tokens, int r) {
+	char** first;
+	for(int i = 0; i < j; i++) {
+		first[i] = tokens[i];	
+	}
+	//TODO: finish plz
 }
 
 void command(char *cmd)
